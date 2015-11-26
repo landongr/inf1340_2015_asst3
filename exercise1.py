b14395246e4ffd8b246f4b102159e43e93309eb2
landongr <graham.cilandon@gmail.com>
2015-11-26 00:27:46 -0500

    Ex1 All code working, Doc String, Touch Up's Needed, Comments Made for over explanation but need touch up

--- exercise1.py
+++ exercise1.py
@@ -76,6 +76,10 @@ def fun(table):
     else:
         print table2
 
+##########################
+##Real code Starts Here!##
+##########################
+
 
 def selection(t, f):
     new_table = []
@@ -91,8 +95,11 @@ def selection(t, f):
     [["A", "B", "C"], [4, 5, 6]]
 
     """
+    # iterate though rows of the table
     for row in t:
+        # checks which rows return True after being run through the function
         if f(row) is True:
+            # If they return True they are appended to a new table
             new_table.append(row)
     return new_table
 
@@ -114,19 +121,29 @@ def projection(t, r):
     [["A", "C"], [1, 3], [4, 6]]
 
     """
+    # iterate though the headings of the schema
     for item in t[0]:
+        # checks if any headings match the attributes in subset r
         if item not in r:
+            # if a heading is not in the subset its index will be stored in a new list
             x = (t[0].index(item))
             new_list.append(x)
+    # iterates though the new list, these are the index points for the columns that will be removed
     for i in new_list:
+        # iterates though each list in the table
         for target in t:
+            # marks any index that matches the column to be removed by changing it to the targeted string
             target[i] = "Marked_for_kill"
+    # iterates though each list in the table again
     for lists in t:
+        # iterates though each list looking for targeted indexes and removes them
         for targets in range(lists.count("Marked_for_kill")):
             lists.remove("Marked_for_kill")
+    # Checks to make sure the attributes in r actually return a list, and will throw an error if the list is empty
     for lists in t:
         if len(lists) == 0:
             raise UnknownAttributeException
+    # Returns the modified table
     else:
         return t
 
@@ -143,14 +160,20 @@ def cross_product(t1, t2):
 
 
     """
+    # Combines and pulls the schema headings to be put on the final table
     new_table = t1[0] + t2[0]
     new2_table = []
+    # Removes the Schema headings from the 2 tables
     del t1[0]
     del t2[0]
+    # Iterates though the lists in both tables
     for list1 in t1:
         for list2 in t2:
+            # Combines the lists for each table (excluding schemas that have been removed)
             t3 = list1 + list2
+            # Appends combined tables to a new list, this stops the lists from overwriting themselves
             new2_table.append(t3)
+    # Inserts the Schema headings back at the top of the table
     new2_table.insert(0,new_table)
     return new2_table
 
